d33078c94195b0b4944708eab88f4c85d8d28c5c
Scott <cadidadi@gmail.com>
2019-01-03 15:53:11 -0700

    20190103 4:01pm

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 app.use('/portfolio', require('./routes/investment'))
 
 //connect to db: crypto1
-mongoose.connect('mongodb://localhost:27017/crypto1', {useNewUrlParser: true}, () => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/crypto1', {useNewUrlParser: true}, () => {
     console.log('Connected to the db - crypto1/investments')
 })
 
